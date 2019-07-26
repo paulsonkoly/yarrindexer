28775173fafdcef5580b5fd4bb15eb22dc934cfa
Paul Sonkoly <sonkoly.pal@gmail.com>
2019-07-26 11:40:55 +0100

    Extracting class/module list

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Lib
   ( Doc
   , extractSection
@@ -7,19 +6,20 @@ module Lib
   )
 where
 
+import qualified Data.Text as Text
 import           Data.Text                      ( Text )
-import           Text.HTML.TagSoup              ( Tag(..) )
+import           Text.HTML.TagSoup              ( Tag(..), (~==) , (~/=))
 import qualified Text.HTML.TagSoup             as TagSoup
-import qualified Text.HTML.TagSoup.Match       as Match
 
 
 type Doc = [Tag Text]
 
 -- | extracts a tag sub tree from a flattened tag list
+--
 -- the returned list is a sublist of the original, start is true on the head,
 -- end is true on the last, and the elements where start is true inside have
 -- matching pairs of ends, and it's the shortest such sublist. In essence if
--- tags are correctly embeded in each other, this finds the closing pair of the
+-- tags are correctly embedded in each other, this finds the closing pair of the
 -- first start and extract the sublist between the two.
 extractSection :: (a -> Bool) -> (a -> Bool) -> [a] -> [a]
 extractSection start end list =
@@ -35,20 +35,39 @@ extractSection start end list =
                 | otherwise = x : go xs n
 
 
+extract :: (a -> Bool) -> (a -> Bool) -> [a] -> [[a]]
+extract start end list =
+  let list' = dropWhile (not . start) list
+      first = extractSection start end list'
+  in  case first of
+        []     -> []
+        first' -> first' : extract start end (drop (length first) list')
+
+
 convert :: Text -> Doc
 convert = TagSoup.parseTags
 
 
 classNames :: Doc -> [Text]
 classNames doc =
-  let classIndex = dropWhile notClassIndex doc
-      entries    = dropWhile notEntries classIndex
-  in  [TagSoup.innerText $ extractDiv entries]
+  let
+    classIndex = dropWhile (~/= "<div id=class-index>") doc
+    entries    = extractDiv $ dropWhile (~/= "<div class=entries>") classIndex
+    paragraphs =
+      extract (~== ("<p>" :: String)) (~== ("</p>" :: String)) entries
+  in
+    map processParagraph paragraphs
  where
-  extractDiv = extractSection isDivOpen isDivClose
-  notClassIndex =
-    not . Match.tagOpen (== "div") (Match.anyAttr (== ("id", "class-index")))
-  notEntries =
-    not . Match.tagOpen (== "div") (Match.anyAttr (== ("class", "entries")))
-  isDivOpen  = Match.tagOpen (== "div") (const True)
-  isDivClose = Match.tagClose (== "div")
+  extractDiv = extractSection (~== "<div>") (~== ("</div>"))
+  processParagraph p =
+    let spn       = extractSection (~== "<span>") (~== "</span>") p
+        anchor    = extractSection (~== "<a>") (~== "</a>") p
+        classType = TagSoup.innerText spn
+        link      = TagSoup.fromAttrib (Text.pack "href") $ head anchor
+        linkText  = TagSoup.innerText anchor
+    in  Text.pack "type : "
+          <> classType
+          <> Text.pack " link : "
+          <> link
+          <> Text.pack " linkText : "
+          <> linkText
